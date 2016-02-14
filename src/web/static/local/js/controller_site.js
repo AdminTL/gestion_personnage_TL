b7a4219190e25809c320205df885b6b9714c990d
Mathieu Benoit <mathben963@gmail.com>
2016-02-14 15:44:04 -0500

    Add page in menu and templates - add facebook feed from traitre-lame page - format js file

--- src/web/static/local/js/controller_site.js
+++ src/web/static/local/js/controller_site.js
@@ -1,11 +1,11 @@
-var machineApp=angular.module('creation_personnage_TL', []);
+var machineApp = angular.module('creation_personnage_TL', []);
 
-machineApp.controller("user_ctrl", ['$scope', function($scope) {
+machineApp.controller("user_ctrl", ['$scope', function ($scope) {
   // update_test_status is an async socket that sends the content of machine.json
-  var data_source="http://" + window.location.host + "/update_user"
+  var data_source = "http://" + window.location.host + "/update_user";
   var socket = new SockJS(data_source);
-  
-  socket.onmessage = function(e) {
+
+  socket.onmessage = function (e) {
     $scope.message = JSON.parse(e.data);
     console.log($scope.message);
     $scope.$apply();
@@ -13,11 +13,31 @@ machineApp.controller("user_ctrl", ['$scope', function($scope) {
 
 }]);
 
-machineApp.controller("page_ctrl", ['$scope', function($scope) {
-    $scope.lstPage = ["User"];
-    $scope.lstSection = [];
-    $scope.activePage = 0;
-    $scope.activeSection = 0;
-    $scope.showLog = false;
-    $scope.showTestLogInfo = false;
+machineApp.controller("page_ctrl", ['$scope', function ($scope) {
+  $scope.lstPage = ["Nouvelle", "Personnage", "Admin"];
+  $scope.lstSection = [];
+  $scope.activePage = 0;
+  $scope.activeSection = 0;
+  $scope.showLog = false;
+  $scope.showTestLogInfo = false;
+
+  /* ######################
+   * function change page
+   * ######################
+   */
+  $scope.changePage = function (event, pageName) {
+    index = $scope.lstPage.indexOf(pageName);
+    if (index >= 0)
+      $scope.activePage = index;
+    else
+      console.err("Cannot find page " + pageName);
+  };
+
+  $scope.changeSection = function (event, sectionName) {
+    index = $scope.lstSection.indexOf(sectionName);
+    if (index >= 0)
+      $scope.activeSection = index;
+    else
+      console.err("Cannot find section " + sectionName);
+  };
 }]);
--- src/web/static/local/js/site.js
+++ src/web/static/local/js/site.js
@@ -3,45 +3,45 @@
  * ############################################################################
  */
 Number.prototype.toHHMMSS = function () {
-    var timestamp = "";
-    var seconds = Math.floor(this),
-        hours = Math.floor(seconds / 3600);
-    seconds -= hours * 3600;
-    var minutes = Math.floor(seconds / 60);
-    seconds -= minutes * 60;
+  var timestamp = "";
+  var seconds = Math.floor(this),
+    hours = Math.floor(seconds / 3600);
+  seconds -= hours * 3600;
+  var minutes = Math.floor(seconds / 60);
+  seconds -= minutes * 60;
 
-    if (hours) {
-        if (hours < 10) {
-            hours = "0" + hours;
-        }
-        timestamp = hours + 'h';
+  if (hours) {
+    if (hours < 10) {
+      hours = "0" + hours;
     }
-    if (minutes) {
-        if (minutes < 10) {
-            minutes = "0" + minutes;
-        }
-        timestamp += minutes + 'm';
+    timestamp = hours + 'h';
+  }
+  if (minutes) {
+    if (minutes < 10) {
+      minutes = "0" + minutes;
     }
-    if (timestamp) {
-        if (seconds < 10) {
-            seconds = "0" + seconds;
-        }
+    timestamp += minutes + 'm';
+  }
+  if (timestamp) {
+    if (seconds < 10) {
+      seconds = "0" + seconds;
     }
-    timestamp += seconds + 's';
-    return timestamp;
-}
+  }
+  timestamp += seconds + 's';
+  return timestamp;
+};
 
 function isDefined(x) {
-    var undefined;
-    return x !== undefined;
+  var undefined;
+  return x !== undefined;
 }
 
 function GetJsonString(str) {
-    json_parse = null;
-    try {
-        json_parse = JSON.parse(str);
-    } catch (e) {
-        // do nothing
-    }
-    return json_parse;
+  json_parse = null;
+  try {
+    json_parse = JSON.parse(str);
+  } catch (e) {
+    console.err("Error parsing json from GetJsonString " + e);
+  }
+  return json_parse;
 }
