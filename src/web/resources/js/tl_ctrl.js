25f90c855da2d8537699c0d42c7d5ededc92cebb
Mathieu Benoit <mathben963@gmail.com>
2016-05-12 02:37:59 -0400

    #19 WIP add example of build form from json information. - add json dependancy - copy example from angular-schema-form

--- src/web/resources/js/tl_ctrl.js
+++ src/web/resources/js/tl_ctrl.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var characterApp = angular.module('creation_personnage_TL', ['ngRoute', 'UserApp']);
+var characterApp = angular.module('creation_personnage_TL', ['ngRoute', 'UserApp', 'schemaForm']);
 
 characterApp.controller("page_ctrl", ['$scope', '$rootScope', '$http', '$location', '$window',
   function ($scope, $rootScope, $http, $location, $window) {
@@ -66,7 +66,7 @@ characterApp.controller("login_ctrl", ['$scope', function ($scope) {
   }
 }]);
 
-characterApp.controller("character_ctrl", ['$scope', '$http', function ($scope, $http) {
+characterApp.controller("character_ctrl", ['$scope', '$q', '$http', '$timeout', function ($scope, $q, $http, $timeout) {
   // var data_source = "http://" + window.location.host + "/update_user";
   // var socket = new SockJS(data_source);
 
@@ -78,6 +78,91 @@ characterApp.controller("character_ctrl", ['$scope', '$http', function ($scope, 
   $scope.characterEdit = true;
   $scope.new_player = false;
   $scope.new_character = false;
+
+  $scope.schema = {
+    "type": "object",
+    "title": "Comment",
+    "properties": {
+      "name": {
+        "title": "Name",
+        "type": "string"
+      },
+      "email": {
+        "title": "Email",
+        "type": "string",
+        "pattern": "^\\S+@\\S+$",
+        "description": "Email will be used for evil."
+      },
+      "comment": {
+        "title": "Comment",
+        "type": "string"
+      }
+    },
+    "required": ["name", "email", "comment"]
+  };
+
+  $scope.form = [
+    {
+      key: 'name',
+      placeholder: 'Anything but "Bob"',
+      $asyncValidators: {
+        'async': function (name) {
+          var deferred = $q.defer();
+          $timeout(function () {
+            if (angular.isString(name) && name.toLowerCase().indexOf('bob') !== -1) {
+              deferred.reject();
+            } else {
+              deferred.resolve();
+            }
+          }, 500);
+          return deferred.promise;
+        }
+      },
+      validationMessage: {
+        'async': "Wooohoo thats not an OK name!"
+      }
+
+    },
+    {
+      key: 'email',
+      placeholder: 'Not MY email',
+      ngModel: function (ngModel) {
+        ngModel.$validators.myMail = function (value) {
+          return value !== 'david.lgj@gmail.com';
+        };
+      },
+      validationMessage: {
+        'myMail': "Thats my mail!"
+      }
+    },
+    {
+      "key": "comment",
+      "type": "textarea",
+      "placeholder": "Make a comment, write 'damn' and check the model",
+      $parsers: [
+        function (value) {
+          if (value && value.replace) {
+            return value.replace(/(damn|fuck|apple)/, '#!@%&');
+          }
+          return value;
+        }
+      ]
+    },
+    {
+      "type": "submit",
+      "style": "btn-info",
+      "title": "OK"
+    }
+  ];
+
+  $scope.model = {};
+
+  $scope.$watch('model', function (value) {
+    if (value) {
+      $scope.prettyModel = JSON.stringify(value, undefined, 2);
+    }
+  }, true);
+
 
   $scope.newPlayer = function () {
     // create empty player with empty character
