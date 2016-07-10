88afde1c7f07aef2e71283977ba910377f5fbd80
Mathieu Benoit <mathben963@gmail.com>
2016-07-10 03:28:50 -0400

    base #23 : Facebook bottom side bar is now only viewable on news page

--- src/web/resources/js/tl_module/manual_ctrl/manual_ctrl.js
+++ src/web/resources/js/tl_module/manual_ctrl/manual_ctrl.js
@@ -5,6 +5,9 @@ characterApp.controller("manual_ctrl", ["$scope", "$q", "$http", "$window", /*"$
   $scope.manual = null;
   $scope._lst_unique_anchor = [];
 
+  $scope.isMobile = function () {
+    return $scope.$parent.active_style == 'Petite personne';
+  };
 
   $scope.formatMenuNavHtml = function (title) {
     return title + " <b class=\"caret\" />";
