a5b1fa97579273bbd00dfac369ef890db44923a9
AlexisBssn <alexis.buisson@hotmail.com>
2018-01-13 16:59:23 -0500

    top navigation

--- src/client_web/src/app/app.module.ts
+++ src/client_web/src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
+import { TabMenuModule } from 'primeng/primeng';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CharacterComponent } from './character/character.component';
 import { LoreComponent } from './lore/lore.component';
 import { ManualComponent } from './manual/manual.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HeaderComponent } from './header/header.component';
 
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'lore', component: LoreComponent },
+  { path: 'character', component: CharacterComponent },
+  { path: 'manual', component: ManualComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   declarations: [
@@ -16,11 +28,16 @@ import { ManualComponent } from './manual/manual.component';
     HomeComponent,
     CharacterComponent,
     LoreComponent,
-    ManualComponent
+    ManualComponent,
+    NotFoundComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
+
+    TabMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
