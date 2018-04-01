126c7cbf0c0bb86ce38989c1985bf042640a2ee5
Mathieu Benoit <mathben963@gmail.com>
2018-04-01 17:34:37 -0400

    [#83] Profile: show status when add or update password - show server error when http error - show timeout error - show validation error from client and server side - show loading when send request - disable button when send request - remove request update profile when add password

--- src/web/resources/js/tl_module/profile_ctrl/profile_ctrl.js
+++ src/web/resources/js/tl_module/profile_ctrl/profile_ctrl.js
@@ -4,13 +4,16 @@
 characterApp.controller("profile_ctrl", ["$scope", "$q", "$http", "$window", /*"$timeout",*/ function ($scope, $q, $http, $window) {
   $scope.model_profile = {
     info: {},
+
     add_password: {
-      old_password: "",
-      new_password: ""
+      password: "",
+      check_password: "",
+      loading: false
     },
     update_password: {
-      password: "",
-      check_password: ""
+      old_password: "",
+      new_password: "",
+      loading: false
     },
     status_password: {
       text: "",
@@ -33,47 +36,28 @@ characterApp.controller("profile_ctrl", ["$scope", "$q", "$http", "$window", /*"
   };
   $scope.update_profile();
 
-  $scope.save_password = function (e) {
-    if ($scope.model_profile.add_password.old_password != "" && $scope.model_profile.add_password.new_password) {
-      var data = {
-        "old_password": hashSha256($scope.model_profile.add_password.old_password),
-        "new_password": hashSha256($scope.model_profile.add_password.new_password)
-      };
-      // send command to server
-      $http({
-        method: "post",
-        url: "/cmd/profile/update_password",
-        headers: {"Content-Type": "application/json; charset=UTF-8"},
-        data: data,
-        timeout: 5000
-      }).then(function (response/*, status, headers, config*/) {
-        console.info(response.data);
-        $scope.model_profile.add_password.old_password = "";
-        $scope.model_profile.add_password.new_password = "";
-
-        $scope.model_profile.status_password.enabled = true;
-        if ("error" in response.data) {
-          $scope.model_profile.status_password.is_error = true;
-          $scope.model_profile.status_password.text = response.data.error;
-        } else if ("status" in response.data) {
-          $scope.model_profile.status_password.is_error = false;
-          $scope.model_profile.status_password.text = response.data.status;
-        } else {
-          $scope.model_profile.status_password.is_error = true;
-          $scope.model_profile.status_password.text = "Unknown error";
-        }
-      });
-    }
-  };
-
   $scope.add_new_password = function (e) {
-    if ($scope.model_profile.update_password.password != "" &&
-      $scope.model_profile.update_password.password == $scope.model_profile.update_password.check_password) {
+    // Validate request is not pending
+    if ($scope.model_profile.add_password.loading) {
+      return;
+    }
 
+    // Validate model field
+    if ($scope.model_profile.add_password.password == "") {
+      $scope.model_profile.status_password.enabled = true;
+      $scope.model_profile.status_password.is_error = true;
+      $scope.model_profile.status_password.text = "The password is empty.";
+    } else if ($scope.model_profile.add_password.password != $scope.model_profile.add_password.check_password) {
+      $scope.model_profile.status_password.enabled = true;
+      $scope.model_profile.status_password.is_error = true;
+      $scope.model_profile.status_password.text = "The password is not identical to the check password.";
+    } else {
       var data = {
-        "password": hashSha256($scope.model_profile.update_password.password)
+        "password": hashSha256($scope.model_profile.add_password.password)
       };
-      // send command to server
+
+      // Send command to server
+      $scope.model_profile.add_password.loading = true;
       $http({
         method: "post",
         url: "/cmd/profile/add_new_password",
@@ -81,10 +65,83 @@ characterApp.controller("profile_ctrl", ["$scope", "$q", "$http", "$window", /*"
         data: data,
         timeout: 5000
       }).then(function (response/*, status, headers, config*/) {
-        console.info(response.data);
-        $scope.model_profile.update_password.password = "";
-        $scope.model_profile.update_password.check_password = "";
+        console.debug(response.data);
 
+        // Reset the loading
+        $scope.model_profile.add_password.loading = false;
+        $scope.model_profile.add_password.password = "";
+        $scope.model_profile.add_password.check_password = "";
+
+        // Show message from server
+        $scope.model_profile.status_password.enabled = true;
+        if ("error" in response.data) {
+          $scope.model_profile.status_password.is_error = true;
+          $scope.model_profile.status_password.text = response.data.error;
+        } else if ("status" in response.data) {
+          $scope.model_profile.status_password.is_error = false;
+          $scope.model_profile.status_password.text = response.data.status;
+          // Add password in info
+          $scope.model_profile.info.password = true;
+        } else {
+          $scope.model_profile.status_password.is_error = true;
+          $scope.model_profile.status_password.text = "Unknown error";
+        }
+      }, function errorCallback(response) {
+        console.error(response);
+
+        if (response.status == -1) {
+          // Timeout
+          $scope.model_profile.update_password.loading = false;
+          $scope.model_profile.status_password.is_error = true;
+          $scope.model_profile.status_password.text = "Timeout resquest.";
+        } else {
+          // Error from server
+          $scope.model_profile.update_password.loading = false;
+          $scope.model_profile.status_password.is_error = true;
+          $scope.model_profile.status_password.text = "Error from server : " + response.status;
+        }
+      });
+    }
+  };
+
+  $scope.update_password = function (e) {
+    // Validate request is not pending
+    if ($scope.model_profile.update_password.loading) {
+      return;
+    }
+
+    // Validate model field
+    if ($scope.model_profile.update_password.old_password == "" || $scope.model_profile.update_password.new_password == "") {
+      $scope.model_profile.status_password.enabled = true;
+      $scope.model_profile.status_password.is_error = true;
+      $scope.model_profile.status_password.text = "The password is empty.";
+    } else if ($scope.model_profile.update_password.old_password == $scope.model_profile.update_password.new_password) {
+      $scope.model_profile.status_password.enabled = true;
+      $scope.model_profile.status_password.is_error = true;
+      $scope.model_profile.status_password.text = "The old password is the same of the new password.";
+    } else {
+      var data = {
+        "old_password": hashSha256($scope.model_profile.update_password.old_password),
+        "new_password": hashSha256($scope.model_profile.update_password.new_password)
+      };
+
+      // Send command to server
+      $scope.model_profile.update_password.loading = true;
+      $http({
+        method: "post",
+        url: "/cmd/profile/update_password",
+        headers: {"Content-Type": "application/json; charset=UTF-8"},
+        data: data,
+        timeout: 5000
+      }).then(function (response/*, status, headers, config*/) {
+        console.debug(response.data);
+
+        // Reset the loading
+        $scope.model_profile.update_password.loading = false;
+        $scope.model_profile.update_password.old_password = "";
+        $scope.model_profile.update_password.new_password = "";
+
+        // Show message from server
         $scope.model_profile.status_password.enabled = true;
         if ("error" in response.data) {
           $scope.model_profile.status_password.is_error = true;
@@ -96,9 +153,20 @@ characterApp.controller("profile_ctrl", ["$scope", "$q", "$http", "$window", /*"
           $scope.model_profile.status_password.is_error = true;
           $scope.model_profile.status_password.text = "Unknown error";
         }
+      }, function errorCallback(response) {
+        console.error(response);
 
-        // Update profile to change view
-        $scope.update_profile();
+        if (response.status == -1) {
+          // Timeout
+          $scope.model_profile.update_password.loading = false;
+          $scope.model_profile.status_password.is_error = true;
+          $scope.model_profile.status_password.text = "Timeout resquest.";
+        } else {
+          // Error from server
+          $scope.model_profile.update_password.loading = false;
+          $scope.model_profile.status_password.is_error = true;
+          $scope.model_profile.status_password.text = "Error from server : " + response.status;
+        }
       });
     }
   };
