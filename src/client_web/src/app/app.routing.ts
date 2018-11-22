3c0197a0b6827afc6477a9f6df56ec9fd05fd443
Mathieu Benoit <mathben963@gmail.com>
2018-11-21 20:30:54 -0500

    wip marche po très bien

--- src/client_web/src/app/app.routing.ts
+++ src/client_web/src/app/app.routing.ts
@@ -6,7 +6,8 @@ import { RegisterComponent } from './register';
 import { AuthGuard } from './_guards';
 
 const appRoutes: Routes = [
-    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    // { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
 
