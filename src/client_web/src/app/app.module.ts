32d466e4e2851412fae7728e975f44bc2baabd49
Mathieu Benoit <mathben963@gmail.com>
2018-11-28 13:32:31 -0500

    WIP add login and register - move routing in app.routing.ts

--- src/client_web/src/app/app.module.ts
+++ src/client_web/src/app/app.module.ts
@@ -1,76 +1,71 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-import { HttpModule } from '@angular/http';
-import { RouterModule, Routes } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import {HttpModule} from '@angular/http';
+import {FormsModule} from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { CharacterFrameComponent } from './character/character-frame.component';
-import { CharacterFormComponent } from './character/form/character-form.component';
-import { CharacterFormSectionComponent } from './character/form/character-form-section.component';
-import { CharacterAttributesComponent } from './character/attributes/character-attributes.component';
-import { CharacterMessagesComponent } from './character/messages/character-messages.component';
-import { CharacterResourcesComponent } from './character/resources/character-resources.component';
-import { CharacterSkillsComponent } from './character/skills/character-skills.component';
-import { CharacterHeaderComponent } from './character/header/character-header.component';
+import {CharacterFrameComponent} from './character/character-frame.component';
+import {CharacterFormComponent} from './character/form/character-form.component';
+import {CharacterFormSectionComponent} from './character/form/character-form-section.component';
+import {CharacterAttributesComponent} from './character/attributes/character-attributes.component';
+import {CharacterMessagesComponent} from './character/messages/character-messages.component';
+import {CharacterResourcesComponent} from './character/resources/character-resources.component';
+import {CharacterSkillsComponent} from './character/skills/character-skills.component';
+import {CharacterHeaderComponent} from './character/header/character-header.component';
 
-import { HomeComponent } from './home/home.component';
-import { LoreComponent } from './lore/lore.component';
-import { ManualComponent } from './manual/manual.component';
-import { DynamicSectionComponent } from './dynamic-section/dynamic-section.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import {AppComponent} from './app.component';
+import {AppRouting} from './app.routing';
+
+import {HomeComponent} from './home/home.component';
+import {LoreComponent} from './lore/lore.component';
+import {JwtInterceptor, ErrorInterceptor} from './_helpers';
+import {ManualComponent} from './manual/manual.component';
+import {DynamicSectionComponent} from './dynamic-section/dynamic-section.component';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
 import {SharedModule} from './shared';
 
 @NgModule({
-    declarations: [
-        CharacterFrameComponent,
-        CharacterFormComponent,
-        CharacterFormSectionComponent,
-        CharacterAttributesComponent,
-        CharacterMessagesComponent,
-        CharacterResourcesComponent,
-        CharacterSkillsComponent,
-        CharacterHeaderComponent,
-        HomeComponent,
-        LoreComponent,
-        DynamicSectionComponent,
-        ManualComponent,
-        NotFoundComponent,
-        AppComponent,
-        LoginComponent
-    ],
-    imports: [
-        CommonModule,
-        HttpClientModule,
-        HttpModule,
-        FormsModule,
-        BrowserModule,
-        SharedModule,
-        BrowserAnimationsModule,
-        RouterModule.forRoot([
-            { path: '', redirectTo: '/home', pathMatch: 'full' },
-            { path: 'home', component: HomeComponent },
-            { path: 'character', redirectTo: '/character/form', pathMatch: 'full'},
-            { path: 'character', component: CharacterFrameComponent, children:[
-                { path: 'form', component: CharacterFormComponent },
-                { path: 'attributes', component: CharacterAttributesComponent },
-                { path: 'skills', component: CharacterSkillsComponent },
-                { path: 'resources', component: CharacterResourcesComponent },
-                { path: 'messages', component: CharacterMessagesComponent }
-            ]},
-            { path: 'lore', component: LoreComponent },
-            { path: 'manual', component: ManualComponent },
-            { path: 'login', component: LoginComponent },
-            { path: '**', component: NotFoundComponent}
-        ]),
-    ],
-    providers: [
-      { provide: 'BASE_URL', useValue: 'http://localhost:8000/' }
-    ],
-    bootstrap: [ AppComponent ]
+  declarations: [
+    CharacterFrameComponent,
+    CharacterFormComponent,
+    CharacterFormSectionComponent,
+    CharacterAttributesComponent,
+    CharacterMessagesComponent,
+    CharacterResourcesComponent,
+    CharacterSkillsComponent,
+    CharacterHeaderComponent,
+    HomeComponent,
+    LoreComponent,
+    DynamicSectionComponent,
+    ManualComponent,
+    NotFoundComponent,
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+  ],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    HttpModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserModule,
+    SharedModule,
+    BrowserAnimationsModule,
+    AppRouting,
+  ],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+
+    {provide: 'BASE_URL', useValue: 'http://localhost:8000/'}
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
