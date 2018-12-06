595b88940c0b78230125b0cba3051c7a5fc95cc7
Mathieu Benoit <mathben963@gmail.com>
2018-12-06 01:13:08 -0500

    WIP test message after content init

--- src/client_web/src/app/home/home.component.ts
+++ src/client_web/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, AfterContentInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 
 import {AlertService, AuthenticationService, LarpemService} from '@app/_services';
@@ -9,10 +9,11 @@ import {Event, Home} from '@app/_models';
   templateUrl: 'home.component.html',
   styleUrls: ['home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterContentInit {
   public isLoaded: Boolean = false;
   public modelHome: Home;
   public nextEvent: Event;
+  private msgInits: String[] = [];
 
   constructor(private http: HttpClient, private authenticationService: AuthenticationService, private alertService: AlertService, private larpemService: LarpemService) {
   }
@@ -26,9 +27,19 @@ export class HomeComponent implements OnInit {
         this.nextEvent = this.modelHome.events[indexNextEvent];
         this.isLoaded = true;
       } else {
-        this.alertService.error('Cannot receive home model data.', true);
+        this.msgInits.push('Cannot receive home model data.');
       }
     });
 
+  }
+
+  ngAfterContentInit() {
+    let msgAppend: string = "";
+    for (let msg of this.msgInits) {
+      msgAppend += msg;
+    }
+    if (msgAppend) {
+      this.alertService.error(msgAppend, true);
+    }
   }
 }
