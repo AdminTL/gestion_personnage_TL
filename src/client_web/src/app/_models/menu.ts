611a9b95ee0d27c57c8a172c249a2f2636f6e8b7
AlexisBuissonLava <alexis.buisson@hotmail.com>
2019-04-28 13:52:01 -0400

    menu support for collapsing and not collapsing menus in any order

--- src/client_web/src/app/_models/menu.ts
+++ src/client_web/src/app/_models/menu.ts
@@ -1,15 +1,9 @@
 ﻿export interface Menu {
   title: string;
-  sibling: SubMenu[];
-  grouped_sibling: SubMenu[];
-}
-
-export interface SubMenu {
-  title: string;
   requiredAdminPermission: boolean;
-  children?: SubMenu[];
-  matIcon?: string;
-  faIcon?: string;
+  subMenu: Menu[];
+  matIcon: string;
+  faIcon: string;
   routerLink: string;
   routerLink_param?: string;
 }
